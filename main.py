35e01f7bd8158f0e4b6ab0129692b065de659858
Ville Talonpoika <talonvi@gmail.com>
2018-10-19 20:22:38 +0300

    Drive test

---
+++ main.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+import keyboard
+from ev3dev2.motor import LargeMotor, OUTPUT_A, OUTPUT_B, OUTPUT_C, OUTPUT_D, SpeedPercent, MoveTank
+from ev3dev2.sensor import INPUT_1
+from ev3dev2.sensor.lego import TouchSensor
+from ev3dev2.led import Leds
+
+# Start
+print()
+print("##########################")
+print("HYPER-AWESOME HACKBOT 2042")
+print("##########################")
+print()
+
+tank_drive = MoveTank(OUTPUT_B, OUTPUT_C)
+
+# drive in a turn for 5 rotations of the outer motor
+# the first two parameters can be unit classes or percentages.
+tank_drive.on_for_rotations(SpeedPercent(50), SpeedPercent(75), 10)
+
+# drive in a different turn for 3 seconds
+tank_drive.on_for_seconds(SpeedPercent(60), SpeedPercent(30), 3)
