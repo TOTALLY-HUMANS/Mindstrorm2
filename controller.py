ad0e58eee2c34304532cabb7708befdb5dda549f
aorin <meeri.rannisto@gmail.com>
2018-10-19 23:39:42 +0300

    controller listens keyboard

--- controller.py
+++ controller.py
@@ -1,4 +1,34 @@
 import rpyc
+from pynput import keyboard
 
-conn = rpyc.connect('192.168.2.2', port=18812)
-conn.root.line_follower()
+class Controller():
+    def __init__(self):
+        self.conn = rpyc.connect('192.168.2.2', port=18812)
+
+    def start(self):
+        self.listener = keyboard.Listener(
+            on_press=self.on_press,
+            on_release=self.on_release
+        )
+        self.listener.start()
+        self.listener.join()
+
+    def stop(self):
+        self.listener.stop()
+
+    def on_press(self, key):
+        self.stop()
+        try:
+            self.conn.root.line_follower()
+        except AttributeError:
+            # key is special key
+            pass
+        self.start()
+
+    def on_release(self, key):
+        #
+        pass
+
+if __name__ == '__main__':
+    c = Controller()
+    c.start()
