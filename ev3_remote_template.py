9ffecd2a471ab40e89b169f83edd1ce43ade42fc
aorin <meeri.rannisto@gmail.com>
2018-10-19 21:55:21 +0300

    add led test to remote test

--- ev3_remote_template.py
+++ ev3_remote_template.py
@@ -1,9 +1,22 @@
 import rpyc
 from rpyc.utils.server import ThreadedServer
+from ev3dev2.led import Leds
 
 class RobotService(rpyc.Service):
-    def exposed_print_msg(self, msg):
-        print(msg)
+    def exposed_speak_message(self, msg):
+        leds = Leds()
+        led_colour_left = "AMBER"
+        led_colour_right = "RED"
+
+        leds.all_off() # Turn all LEDs off
+        sleep(1)
+
+        # Set both pairs of LEDs to amber
+        leds.set_color('LEFT', led_colour_left)
+        print("\nLED color set to " + led_colour_left)
+        leds.set_color('RIGHT', led_colour_right)
+        print("\nLED color set to " + led_colour_right)
+        sleep(4)
 
 if __name__ == '__main__':
     s = ThreadedServer(RobotService, port=18812)
